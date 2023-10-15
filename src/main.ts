725ca0849dce4dbbb3274c2021e8f8d23fa7f2ce
brutalzinn <robertocpaes@gmail.com>
2023-10-15 10:15:08 -0300

    add port console

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = 3000 || process.env.PORT
+  console.log(`Starting at ${PORT}`)
   await app.listen(PORT);
 }
 bootstrap();
